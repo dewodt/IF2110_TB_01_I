afd883e3114cc118258e8b301f6d6bb3193e2e59
Bbennn <sihombingbenjamin2004@gmail.com>
2023-10-30 19:43:52 +0700

    update: inisialisasi

--- main.c
+++ main.c
@@ -1,4 +1,18 @@
-int main()
-{
+#include <stdio.h>
+int main() {
+    printf(".______    __    __  .______      .______    __  .______ \n");
+    printf("|   _  \\  |  |  |  | |   _  \\     |   _  \\  |  | |   _  \\\n");
+    printf("|  |_)  | |  |  |  | |  |_)  |    |  |_)  | |  | |  |_)  |\n");
+    printf("|   _  <  |  |  |  | |      /     |   _  <  |  | |      /     \n");
+    printf("|  |_)  | |  `--'  | |  |\\  \\----.|  |_)  | |  | |  |\\  \\----.\n");
+    printf("|______/   \\______/  | _| `._____||______/  |__| | _| `._____|\n");
+    printf("Selamat datang di BurBir.\n");
+    printf("Aplikasi untuk studi kualitatif mengenai perilaku manusia dengan menggunakan \n");
+    printf("metode (pengambilan data berupa) Focused Group Discussion kedua di zamannya.\n");
+    printf("Silahkan masukan folder konfigurasi untuk dimuat: \n");
+    printf("File konfigurasi berhasil dimuat! Selamat berkicau!\n");
+    printf(">>\n");
+    printf("\n");
+    printf("Hello, World!");
     return 0;
 }

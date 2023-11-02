c5aba3e2ad00b67da82dfa4dbebc8e72d8b8087a
denoseu <denisefeliciatiowanni@gmail.com>
2023-11-02 18:09:49 +0700

    update pengguna.c

--- pengguna.c
+++ pengguna.c
@@ -72,9 +72,9 @@ void DAFTAR(ListStatik *pengguna, boolean isLoggedin) {
         }
 
         int idx = listLength(*pengguna);
-        printList(*pengguna);
-        printf("\n");
-        printf("listlength: %d\n", idx);
+        // printList(*pengguna);
+        // printf("\n");
+        // printf("listlength: %d\n", idx);
         
         Word username_temp;
         printf("Masukkan nama: ");
@@ -182,6 +182,7 @@ int main() {
         }
         printList(pengguna);
         printf("\n");
+        printf("%d\n", listLength(pengguna));
 
         printf(">> ");
         baca(&command);

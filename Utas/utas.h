6c864cd64370145c9a240e8b6b42b6d7a354d527
Rvans <shafiq.irvansyah@gmail.com>
2023-11-03 19:07:32 +0700

    Updated File Structure

--- Utas/utas.h
+++ Utas/utas.h
@@ -1,7 +1,8 @@
-#include "../ADT/time/time.h"
-#include "../ADT/time/datetime.c"
-#include "../ADT/wordmachine/wordmachine.h"
-#include "../ADT/listlinier/listlinier.h"
+#include "ADT/time/time.h"
+#include "ADT/time/datetime.c"
+#include "ADT/listlinier/listlinierForUtas.h"
+#include "ADT/wordmachine/wordmachine.h"
+#include "kicauan.h"
 
 #ifndef UTAS_H
 #define UTAS_H
@@ -9,9 +10,8 @@
 // Bentukan Utas
 typedef struct
 {
+    Kicauan *tweet;
     int IDUtas;
-    DATETIME datetime;
-    Word text;
     Address sambungan;
 } UTAS;
 
@@ -19,34 +19,5 @@ typedef struct
 #define Text(u) (u).text
 #define DateTime(u) (u).datetime
 #define IDUtas(u) (u).IDUtas
-
-// Modified List Dinamis Untuk Menyimpan Utas
-typedef int IdxType;
-typedef struct
-{
-    UTAS *buffer;
-    int nEff;
-    int capacity;
-} ListDin;
-
-#define NEFF(l) (l).nEff
-#define BUFFER(l) (l).buffer
-#define ELMT(l, i) (l).buffer[i]
-#define CAPACITY(l) (l).capacity
-
-// Modified List Linier Untuk Kicauan Sambungan
-/* Definisi Node : */
-
-typedef struct node *Address;
-typedef struct node
-{
-    Word info;
-    Address next;
-} Node;
-
-#define INFO(p) (p)->info
-#define NEXT(p) (p)->next
-
-Address newNode(Word text);
 
 #endif

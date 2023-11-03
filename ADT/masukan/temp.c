a6061532621570d2b7da9793065d52058e9597bb
Bbennn <sihombingbenjamin2004@gmail.com>
2023-11-04 00:11:58 +0700

    new: bacaInt

---
+++ ADT/masukan/temp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "masukanint.h"
+
+int main(){
+    int a,b,c;
+    bacaInt(&a);
+    printf("%d\n",a);
+    bacaInt(&b);
+    printf("%d\n",b);
+    bacaInt(&c);
+    printf("%d\n",c);
+    return 0;
+}

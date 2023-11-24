1c4fbe30c8144ac6700147c8381d7c22421dbb34
Rvans <shafiq.irvansyah@gmail.com>
2023-11-24 20:24:10 +0700

    update: driver listlinier utas

--- ADT/listlinierUtas/mlistlinierUtas.c
+++ ADT/listlinierUtas/mlistlinierUtas.c
@@ -1,28 +1,139 @@
 #include <stdio.h>
 #include "listlinierForUtas.h"
-#include "../masukan/masukan.c"
 #include "../tree/tree.h"
-#include "../modifiedliststatik/modifiedliststatik.h"
-
-char currentChar;
-boolean EOP;
+#include "../masukan/masukan.h"
+#include "../masukan/masukanint.h"
+#include "../charmachine/charmachine.h"
+#include "../kicauan/kicauan.h"
 
 int main()
 {
-    threads sambungan;
+
+    MASUKAN teks;
+    MASUKANint type, teksint;
+
+    char auth[20];
+    bacaInt(&type);
+
+    DATETIME time1;
+    Month(time1) = 11;
+    Year(time1) = 2023;
+    Day(time1) = 23;
+    Hour(Time(time1)) = 9;
+    Minute(Time(time1)) = 23;
+    Second(Time(time1)) = 26;
+
+    DATETIME time2;
+    Month(time2) = 11;
+    Year(time2) = 2023;
+    Day(time2) = 15;
+    Hour(Time(time2)) = 9;
+    Minute(Time(time2)) = 23;
+    Second(Time(time2)) = 26;
+
+    DATETIME time3;
+    Month(time3) = 10;
+    Year(time3) = 2023;
+    Day(time3) = 19;
+    Hour(Time(time3)) = 19;
+    Minute(Time(time3)) = 23;
+    Second(Time(time3)) = 26;
+
+    threads linier;
+    CreateThreads(&linier);
+
+    User joseph, ucup;
+    strcpy(ucup.username, "Yusuf");
+    strcpy(joseph.username, "Joseph");
+
+    // Buat Kicauan
+    AddressKicauan kicau1, kicau2;
+
+    // ID
+    ID(InfoKicauan(kicau1)) = 1;
+    // Like
+    LIKE(InfoKicauan(kicau1)) = 77;
+    // Author
+    AUTHOR(InfoKicauan(kicau1)) = &joseph;
+    // Datetime
+    DATETIME(InfoKicauan(kicau1)) = time1;
+    // Text
+    strcpy(TEXT(InfoKicauan(kicau1)), "AKU JOSEPH");
+
+    // ID
+    ID(InfoKicauan(kicau2)) = 2;
+    // Like
+    LIKE(InfoKicauan(kicau2)) = 70;
+    // Author
+    AUTHOR(InfoKicauan(kicau2)) = &ucup;
+    // Datetime
+    DATETIME(InfoKicauan(kicau2)) = time1;
+    // Text
+    strcpy(TEXT(InfoKicauan(kicau2)), "AKU YUDUF");
+
     UTAS utas;
-    int i;
-    char input[280];
 
-    // Membuat Kicauan sambungan
-    CreateThreads(&sambungan);
-    for (i = 0; i < 4; i++)
+    switch (type)
     {
-        printf("Masukkan Kicauan Sambungan ke-%d\n", i + 1);
-        scanf("%s", &input);
-        insertLastThreads(&sambungan, input, getCurrTime());
-    }
+    case 1:
 
-    printf("\nKicauan Sambungan:\n\n");
-    displayThreads(sambungan, "Random Man");
+        // Testing membuat kicauan sambungan baru
+
+        strcpy(auth, "Messi");
+        baca(&teks);
+
+        CreateThreads(&linier);
+
+        insertLastThreads(&linier, MASUKANToStr(teks), time1);
+
+        displayThreads(linier, auth);
+        break;
+
+    case 2:
+        // Testing Menghapus kicauan sambungan pada index tertentu
+        strcpy(auth, "Stark");
+
+        // Input 3 Kicauan Sambungan
+        baca(&teks);
+        insertLastThreads(&linier, MASUKANToStr(teks), time2);
+
+        baca(&teks);
+        insertLastThreads(&linier, MASUKANToStr(teks), time2);
+
+        baca(&teks);
+        insertLastThreads(&linier, MASUKANToStr(teks), time2);
+        displayThreads(linier, auth);
+
+        // Hapus Kicauan Sambungan Pada Indeks Tertentu
+        bacaInt(&teksint);
+        deleteAtThreads(&linier, teksint);
+
+        // Menampilkan Utas
+        displayThreads(linier, auth);
+        break;
+    case 3:
+
+        // Menambah Kicauan Sambungan Pada Indeks Tertentu
+        bacaInt(&teksint);
+        baca(&teks);
+        insertAtThreads(&linier, MASUKANToStr(teks), teksint);
+
+        displayThreads(linier, auth);
+
+        printf("%d", lengthThreads(linier));
+        break;
+
+    case 4:
+        //  Membuat Utas
+
+        CreateUtas(&utas, &kicau1, 1);
+        for (int i = 0; i < 3; i++)
+        {
+            baca(&teks);
+            SambungUtasLast(&utas, MASUKANToStr(teks), time3);
+        }
+        displayUtas(utas);
+    default:
+        break;
+    }
 }
